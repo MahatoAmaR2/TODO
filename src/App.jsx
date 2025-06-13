8ef1a440846764bca37859c823ed561e490ff563
MahatoAmaR2 <bauaa476@gmail.com>
2025-06-13 20:25:16 +0530

    Install Raect

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      className="text-2xl text-center text-white
+    bg-black"
+    >
+      Har Har Mahadev
+    </div>
+  );
+};
+
+export default App;
